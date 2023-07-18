bbf96ecc34b9f4d9221d5d078521c74ef446d712
sevase <kotaseva8@gmail.com>
2023-07-18 20:45:06 +0300

    фигурки нарисовал, текстик сделал

--- videos_draw.py
+++ videos_draw.py
@@ -8,7 +8,16 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
 
-    cv2.imshow('frame', frame)
+    img = cv2.line(frame, (0, 0), (width, height), (43, 0, 0), 10) #линия1 # (0)frame - наше изображение, (1)координата верхнего левого угла, (2)нижнего правого угла, (3)цвет, (4)толщина
+    img = cv2.line(img, (0, width), (height, 0), (0, 24, 200), 5) #линия2
+    img = cv2.rectangle(img, (0, 0), (50, 100), (210, 23, 200), 10) #прямоугольник # (1)координата верхней правой точки, (2)координата нижней левой точки, после прямоугольник заполняется сам
+    img = cv2.circle(img, (300, 300), 40, (32, 200, 200), -1) #круг # (1)координата где он стоит, (2)радиус круга (4)-1 заполняетя круг цветом
+
+    # текс
+    font = cv2.FONT_HERSHEY_PLAIN
+    img = cv2.putText(img, 'Sevase is Great!!!', (200, height-10), font, 2, (0, 0, 0), 3, cv2.LINE_AA) #текст # (4) маштаб шрифта
+
+    cv2.imshow('frame', img)
 
     if cv2.waitKey(1) == ord('q'):
         break
