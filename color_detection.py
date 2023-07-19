e3fd313ef6c47682509d034132823c82f63acd96
sevase <kotaseva8@gmail.com>
2023-07-19 12:52:57 +0300

    подправил обнаружение синего цвета

--- color_detection.py
+++ color_detection.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(1)
-realcap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+
 
 while True:
     # real cam
-    ret_real, real_frame = realcap.read()
+    ret_real, real_frame = cap.read()
 
 
     # detect blue
@@ -18,17 +18,12 @@ while True:
     lower_blue = np.array([90, 50, 50]) # светлый синий
     upper_blue = np.array([130, 250, 250]) # темно синий
 
-    mask = cv2.inRange(hsv, lower_blue, upper_blue) # маска - часть кадра , в которой только синие оттенки
+    mask = cv2.inRange(hsv, lower_blue, upper_blue) # маска - часть кадра, в которой только серые оттенки
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    # Make real_frame semi-transparent
-    alpha = 0.5  # Задайте желаемый коэффициент прозрачности (от 0.0 до 1.0)
-    beta = 1.0 - alpha
-    overlay = real_frame.copy()
-    cv2.addWeighted(overlay, alpha, real_frame, beta, 0, real_frame)
-
-    cv2.imshow('detection BLUE', result)
-    cv2.imshow('real', real_frame)
+    cv2.imshow('detection BLUE', result) # вывод только синих объектовый
+    cv2.imshow('real', real_frame) # вывод реального изобр
+    cv2.imshow('mask', mask) # вывод маски
 
     if cv2.waitKey(1) == ord('q'):
         break
